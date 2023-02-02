691ca52837c6727c3dd0e5233fe10cfd816063f4
Clément <clement.llobet@jasparo.fr>
2023-02-02 23:17:23 +0100

    add self keyword to ContactModel methods

--- contact_manager/contact_model.py
+++ contact_manager/contact_model.py
@@ -6,21 +6,21 @@ class ContactModel:
         self.surname = surname
         self.email = email
 
-    def chek_name(name):
+    def check_name(self, name):
         if (name.len <= 2):
             print_name_response = print("Votre nom doit avoir plus de 2 caractères. Veuillez réessayer.")
             return print_name_response
 
         return name
 
-    def check_surname(surname):
+    def check_surname(self, surname):
         if (surname.len <= 2):
             print_surname_response = print("Votre prénom doit avoir plus de 2 caractères. Veuillez réessayer.")
             return print_surname_response
 
         return surname
 
-    def check_email(email):
+    def check_email(self, email):
         regex = r'/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/'
 
         if (re.fullmatch(regex, email)):
