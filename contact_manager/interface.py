ebd9a097a4c6966ff03ce084592a05f544d4e5f8
Clément <clement.llobet@tutanota.com>
2023-02-06 20:17:50 +0100

    create app interface

---
+++ contact_manager/interface.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from contact_model import ContactModel
+
+window = Tk()
+
+window.geometry('1200x600')
+window.title('Gestionnaire de contacts')
+window.configure(background = '#FFF')
+# window.resizable(height=False, width=False)
+
+title_label = Label(window, text='Gestionnaire de contact')
+title_label.pack(pady=50)
+
+
+def validate_values():
+    contact_name = name_input_value.get()
+    contact_surname = surname_input_value.get()
+    contact_email = email_input_value.get()
+    new_contact_model = ContactModel(contact_name, contact_surname, contact_email)
+    print(new_contact_model.surname, new_contact_model.name, new_contact_model.email)
+
+
+surname_input_value = StringVar()
+name_input_value = StringVar()
+email_input_value = StringVar()
+
+surname_label = Label(window, text="Prénom").pack()
+surname_input = Entry(window, textvariable=surname_input_value).pack()
+error_surname_message = Label(window, text="")
+
+name_label =  Label(window, text="Nom").pack()
+name_input = Entry(window, textvariable=name_input_value).pack()
+error_name_message = Label(window, text="")
+
+email_label = Label(window, text="Email").pack()
+email_input = Entry(window, textvariable=email_input_value).pack()
+error_email_message = Label(window, text="")
+
+validation_button = Button(window, text='Valider', command=validate_values).pack()
+
+
+window.mainloop()
